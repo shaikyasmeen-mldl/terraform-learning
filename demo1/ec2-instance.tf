cda13b6256e42feb629ee628faa03d104984fdcf
Shaik Yasmeen <66788030+shaikyasmeen-mldl@users.noreply.github.com>
2021-06-18 14:34:06 +0530

    Update ec2-instance.tf

--- demo1/ec2-instance.tf
+++ demo1/ec2-instance.tf
@@ -15,10 +15,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Yasu"
+    Name = "Arjun"
   }
 }

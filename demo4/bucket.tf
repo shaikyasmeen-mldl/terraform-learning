3b90cfefe40c73ccfcb8035442c91fc5f6a1751b
Shaik Yasmeen <66788030+shaikyasmeen-mldl@users.noreply.github.com>
2021-06-18 16:06:00 +0530

    Update bucket.tf

--- demo4/bucket.tf
+++ demo4/bucket.tf
@@ -1,19 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
 
-  required_version = ">= 0.14.9"
-}
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
 
   tags = {
-    Name        = "Yasmeenbucket"
+    Name        = "yyy456"
     Environment = "Dev"
   }
 }
